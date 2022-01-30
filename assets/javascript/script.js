4a996a5743ceb43879bd79e5300132023900d3c4
Erock FaQue <eric.kemerer@gmail.com>
2022-01-29 23:11:46 -0500

    added time-sensitive color-coding, button text, need save/load functions still (js progress as notes)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,28 +1,70 @@
+var tasks = {};
+
+//moment.js day and date
 const currentDate = 
 moment().format('dddd, MMMM Do YYYY');
 document.getElementById("currentDay").textContent=currentDate;
 
-var hour1 = document.getElementById("hour1");
-var hour2 = document.getElementById("hour2");
-var hour3 = document.getElementById("hour3");
-var hour4 = document.getElementById("hour4");
-var hour5 = document.getElementById("hour5");
-var hour6 = document.getElementById("hour6");
-var hour7 = document.getElementById("hour7");
-var hour8 = document.getElementById("hour8");
-var hour9 = document.getElementById("hour9");
+//use $? also array for save function or color change?
 
-//use $? also array^?
+//moment.js hour format
+let currentHour = parseInt(moment().format('H'));
+//check hour is up and current
+console.log(currentHour)
 
-var description= {};
+//var loadTasks = function() {
+    //tasks = JSON.parse(localStorage.getItem("tasks"));
 
-//let currentHour = [
-    //{id="9", id="10", id="11", id="12", id="13", id="14", id="15", id="16", id="17"}
-//];
+    //if (!tasks) {
+        //tasks = {
+          //timeBlockEl: [],
+        //};
+      //}
+
+        // loop over object properties
+  //$.each(tasks, function(list, arr) {
+    // then loop over sub-array
+    //arr.forEach(function(task) {
+      //createTask(task.text, task.date, list);
+    //});
+  //});
+//};
+  
+
+//save function here
 
 var saveBtn = function() {
-    localStorage.setItem("description", JSON.stringify(description));
+    localStorage.setItem("textarea", JSON.stringify(description));
   };
 
 
+
+var checkTime = function () {
+
+    var currentHour = moment().format('H');
+
+    var timeBlockEl = $(".textarea");
+
+    for (var i = 0 ; i < timeBlockEl.length ; i++) {
+
+        var elementID = timeBlockEl[i].id;
+
+        //ask about $ vs document.getElementById here
+        var tenseID = document.getElementById(timeBlockEl[i].id)
+
+        //remove any old classes from element
+        $(timeBlockEl[i].id).removeClass(".present .past .future");
+
+        if (elementID < currentHour) {
+            $(tenseID).addClass("past");
+        } else if (elementID > currentHour) {
+            $(tenseID).addClass("future");
+        } else {
+            $(tenseID).addClass("present");
+        }
+    }
+}
+
+//run checkTime on interval to update colors
+setInterval(checkTime(), (1000 * 60) * 5);
 
